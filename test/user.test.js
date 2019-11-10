b25e404d88e148bb1a371dc21ea7af5ea85644c5
Lorenzo Fernandez <lorenzo.kniss@gmail.com>
2019-11-10 18:16:05 -0300

    Validated every request and kind of error based on the Model Validator

--- test/user.test.js
+++ test/user.test.js
@@ -11,6 +11,11 @@ var should = chai.should();
 
 chai.use(chaiHttp);
 
+const nameMinLength = 3;
+const nameMaxLenght = 50;
+const passwordMinLenght = 3;
+const passwordMaxLenght = 255;
+
 describe('Users Unit Tests', function () {
 
     // Empty the database before each tests
@@ -44,7 +49,54 @@ describe('Users Unit Tests', function () {
 
     describe('/POST /users', () => {
 
-        it('it should returns an error due to the data validation', (done) => {
+        it('it should returns an error because the "name" field could not be empty', (done) => {
+
+            let user = {
+                name: "",
+            };
+
+            chai.request(server)
+                .post('/users')
+                .set('content-type', 'application/json')
+                .send(user)
+                .end((err, res) => {
+
+                    if (err) done(err);
+
+                    res.should.have.status(400);
+                    res.error.should.have.property('text');
+
+                    assert.equal(res.error.text, '"name" is not allowed to be empty');
+                    
+                    done();
+                });
+        });
+
+        it('it should returns an error because the "name" field is required', (done) => {
+
+            let user = {
+                email: "email@email.com",
+                password: "aBcDef"
+            };
+
+            chai.request(server)
+                .post('/users')
+                .set('content-type', 'application/json')
+                .send(user)
+                .end((err, res) => {
+
+                    if (err) done(err);
+
+                    res.should.have.status(400);
+                    res.error.should.have.property('text');
+
+                    assert.equal(res.error.text, '"name" is required');
+                    
+                    done();
+                });
+        });
+
+        it('it should returns an error because the "name" field is too short', (done) => {
 
             let user = {
                 name: "Zé"
@@ -58,9 +110,207 @@ describe('Users Unit Tests', function () {
 
                     if (err) done(err);
 
-                    res.should.have.status(422);
-                    res.should.have.status(422);
+                    res.should.have.status(400);
+                    res.error.should.have.property('text');
 
+                    assert.equal(res.error.text, `"name" length must be at least ${nameMinLength} characters long`);
+                    
+                    done();
+                });
+        });
+
+        it('it should returns an error because the "name" field is too long', (done) => {
+
+            let user = {
+                name: "m1FRZnOaIgthORQT018JEZN1MwDbYtgIPf8oHwfbutDRjOJpP1X"
+            };
+
+            chai.request(server)
+                .post('/users')
+                .set('content-type', 'application/json')
+                .send(user)
+                .end((err, res) => {
+
+                    if (err) done(err);
+
+                    res.should.have.status(400);
+                    res.error.should.have.property('text');
+
+                    assert.equal(res.error.text, `"name" length must be less than or equal to ${nameMaxLenght} characters long`);
+                    
+                    done();
+                });
+        });
+
+        it('it should returns an error because the "email" field could not be empty', (done) => {
+
+            let user = {
+                name: "Random User",
+                email: "",
+                password: "AbcDef"
+            };
+
+            chai.request(server)
+                .post('/users')
+                .set('content-type', 'application/json')
+                .send(user)
+                .end((err, res) => {
+
+                    if (err) done(err);
+
+                    res.should.have.status(400);
+                    res.error.should.have.property('text');
+
+                    assert.equal(res.error.text, '"email" is not allowed to be empty');
+                    
+                    done();
+                });
+        });
+
+        it('it should returns an error because the "email" field is required', (done) => {
+
+            let user = {
+                name: "Random User",
+                password: "m1FRZnOaIgthORQT018JEZN1MwDbYtgIPf8oHwfbutDRjOJpP1X"   
+            };
+
+            chai.request(server)
+                .post('/users')
+                .set('content-type', 'application/json')
+                .send(user)
+                .end((err, res) => {
+
+                    if (err) done(err);
+
+                    res.should.have.status(400);
+                    res.error.should.have.property('text');
+
+                    assert.equal(res.error.text, '"email" is required');
+                    
+                    done();
+                });
+        });
+
+        it('it should returns an error because the "email" address is invalid', (done) => {
+
+            let user = {
+                name: "Random User",
+                email: "randomEmail",
+                password: "m1FRZnOaIgthORQT018JEZN1MwDbYtgIPf8oHwfbutDRjOJpP1X"  
+            };
+
+            chai.request(server)
+                .post('/users')
+                .set('content-type', 'application/json')
+                .send(user)
+                .end((err, res) => {
+
+                    if (err) done(err);
+
+                    res.should.have.status(400);
+                    res.error.should.have.property('text');
+
+                    assert.equal(res.error.text, '"email" must be a valid email');
+                    
+                    done();
+                });
+        });
+
+        it('it should returns an error because the "password" field could not be empty', (done) => {
+
+            let user = {
+                name: "Random User",
+                email: "email@email.com",
+                password: ""
+            };
+
+            chai.request(server)
+                .post('/users')
+                .set('content-type', 'application/json')
+                .send(user)
+                .end((err, res) => {
+
+                    if (err) done(err);
+
+                    res.should.have.status(400);
+                    res.error.should.have.property('text');
+
+                    assert.equal(res.error.text, '"password" is not allowed to be empty');
+                    
+                    done();
+                });
+        });
+
+        it('it should returns an error because the "password" field is required', (done) => {
+
+            let user = {
+                name: "Random User",
+                email: "email@email.com"
+            };
+
+            chai.request(server)
+                .post('/users')
+                .set('content-type', 'application/json')
+                .send(user)
+                .end((err, res) => {
+
+                    if (err) done(err);
+
+                    res.should.have.status(400);
+                    res.error.should.have.property('text');
+
+                    assert.equal(res.error.text, '"password" is required');
+                    
+                    done();
+                });
+        });
+
+        it('it should returns an error because the "password" field is too short', (done) => {
+
+            let user = {
+                name: "Random User",
+                email: "email@email.com",
+                password: "zH"
+            };
+
+            chai.request(server)
+                .post('/users')
+                .set('content-type', 'application/json')
+                .send(user)
+                .end((err, res) => {
+
+                    if (err) done(err);
+
+                    res.should.have.status(400);
+                    res.error.should.have.property('text');
+
+                    assert.equal(res.error.text, `"password" length must be at least ${passwordMinLenght} characters long`);
+                    
+                    done();
+                });
+        });
+
+        it('it should returns an error because the "password" field is too long', (done) => {
+
+            let user = {
+                name: "Random User",
+                email: "email@email.com",
+                password: "ZWRMsn6FPwqfT6s5blNmChZda9sIMnp91yzr5uGnbSvBI6aGjeJdiKrN9kn4F5ST1C1ylf8MbyRBgbCqQ9W0sxPTseslD4hzdmcVQymOUdVyuev5KvEDEqnAmEHmkEdPNEl7hCo6kOes12dRap1x2mk2NM6jRNBvzLSKMGaKAorkb4YFpOzwFFiQNDKlKypVBzFnDujScHAMgaWHDXNFDoRwBLKpPqFvZOoqjioNp0ootBgsgRuklNSHFyZeeCnQ                "  
+            };
+
+            chai.request(server)
+                .post('/users')
+                .set('content-type', 'application/json')
+                .send(user)
+                .end((err, res) => {
+
+                    if (err) done(err);
+
+                    res.should.have.status(400);
+                    res.error.should.have.property('text');
+
+                    assert.equal(res.error.text, `"password" length must be less than or equal to ${passwordMaxLenght} characters long`);
+                    
                     done();
                 });
         });
